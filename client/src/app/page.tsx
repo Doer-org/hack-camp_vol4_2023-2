bffea569a72479c6db1276db0aff7873315cccda
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-20 23:47:42 +0900

    feat(client): useEnv

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
 import { DebugButton } from "./_ui";
+import { useEnv } from "@/utils/env";
 export default function Home() {
+  const { clientURL } = useEnv();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <Link href="/api/auth/login"> Login </Link>
+        <Link href="/api/auth/login" prefetch={false}>
+          Login
+        </Link>
       </div>
       <div>
-        <Link href="/api/auth/login"> Logout </Link>
+        <Link href="/api/auth/login" prefetch={false}>
+          Logout
+        </Link>
       </div>
       <div>
         <DebugButton
           label="Health Check Button"
-          url="http://localhost:3000/api/health"
+          url={`${clientURL}/api/health`}
         />
       </div>
       <div>
-        <DebugButton
-          label="Get Profile"
-          url="http://localhost:3000/api/auth/me"
-        />
+        <DebugButton label="Get Profile" url={`${clientURL}/api/auth/me`} />
       </div>
     </main>
   );
