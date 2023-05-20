94de0e314425debd96e1af28b7f0897bdfbed469
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-20 18:33:24 +0900

    chore(client): use Link

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import { DebugButton } from "./_ui";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <a href="/api/auth/login"> Login </a>
+        <Link href="/api/auth/login"> Login </Link>
       </div>
       <div>
-        <a href="/api/auth/login"> Logout </a>
+        <Link href="/api/auth/login"> Logout </Link>
       </div>
       <div>
         <DebugButton
