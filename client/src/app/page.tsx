d79af609b37be43df57bc3fa07922bc6ce809227
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-20 18:18:19 +0900

    chore(client): auth check page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,23 @@
 import { DebugButton } from "./_ui";
-import { Button } from "@/ui";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
+        <a href="/api/auth/login"> Login </a>
+      </div>
+      <div>
+        <a href="/api/auth/login"> Logout </a>
+      </div>
+      <div>
         <DebugButton
           label="Health Check Button"
           url="http://localhost:3000/api/health"
+        />
+      </div>
+      <div>
+        <DebugButton
+          label="Get Profile"
+          url="http://localhost:3000/api/auth/me"
         />
       </div>
     </main>
