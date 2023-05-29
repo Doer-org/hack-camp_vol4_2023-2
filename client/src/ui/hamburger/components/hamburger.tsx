a140ef4a16dfced9aa7f15c3d9a24fc8f6cf2272
yach36 <yach0306@gmail.com>
2023-05-29 20:29:52 +0900

    feat(client): update hamburger component

--- client/src/ui/hamburger/components/hamburger.tsx
+++ client/src/ui/hamburger/components/hamburger.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import * as styles from "../styles/hamburger.css";
 
-type Props = {
-  onClick?: () => void;
-};
-
 const _Hamburger = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const barClassList = [styles.barStyle, open && styles.barOpenStyle];
+
   return (
-    <div className={styles.boxStyle}>
-      <span className={styles.barStyle}></span>
-      <span className={styles.barStyle}></span>
-      <span className={styles.barStyle}></span>
+    <div className={styles.boxStyle} onClick={handleClick}>
+      <span className={barClassList.join(" ")}></span>
+      <span className={barClassList.join(" ")}></span>
+      <span className={barClassList.join(" ")}></span>
     </div>
   );
 };
--- client/src/ui/hamburger/styles/hamburger.css.ts
+++ client/src/ui/hamburger/styles/hamburger.css.ts
@@ -2,7 +2,9 @@ import { style } from "@vanilla-extract/css";
 
 export const boxStyle = style({
   width: "24px",
+  height: "24px",
   cursor: "pointer",
+  position: "relative",
 });
 
 export const barStyle = style({
@@ -10,9 +12,37 @@ export const barStyle = style({
   width: "100%",
   height: "4px",
   background: "#0b002a",
+  position: "absolute",
   selectors: {
-    "&:not(:first-child)": {
-      marginTop: "6px",
+    "&:nth-child(1)": {
+      top: "0",
+    },
+    "&:nth-child(2)": {
+      top: "50%",
+      transform: "translateY(-50%)",
+    },
+    "&:nth-child(3)": {
+      bottom: "0",
+    },
+  },
+});
+
+export const barOpenStyle = style({
+  width: "32px",
+  height: "2px",
+  selectors: {
+    "&:nth-child(1)": {
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%) rotate(45deg)",
+    },
+    "&:nth-child(2)": {
+      display: "none",
+    },
+    "&:nth-child(3)": {
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%) rotate(-45deg)",
     },
   },
 });
