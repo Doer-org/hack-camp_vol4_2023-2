b7ac0cb819239f0ab8750516651d7d5863b10b2e
yuuugaaa <yuuugaaa.321@gmail.com>
2023-05-30 21:58:39 +0900

    サイズ調整

--- client/src/ui/avator/styles/avator.css.ts
+++ client/src/ui/avator/styles/avator.css.ts
@@ -12,7 +12,7 @@ export const imageStyle = style({
 });
 
 export const size = styleVariants({
-  tiny: { width: "32px", height: "32px" },
+  tiny: { width: "28px", height: "28px" },
   small: { width: "42px", height: "42px" },
   medium: { width: "52px", height: "52px" },
 });
