462cbaca9cfea2eb5c8141aa7d84ce1b3ebc0744
yach36 <yach0306@gmail.com>
2023-06-01 05:02:49 +0900

    chore(client): artist response type

--- client/src/pages/api/spotify/search/index.ts
+++ client/src/pages/api/spotify/search/index.ts
@@ -1,11 +1,12 @@
 import { spotifyApiClient } from "../client";
+import { ArtistResponse } from "./types";
 
 export const searchArtist = async (q: string) => {
   const data = await spotifyApiClient.get(
     `${process.env.NEXT_PUBLIC_SPOTIFY_API_BASE_URL}/search?q=${q}&type=artist&limit=10`
   );
   if (data.type === "error") return data;
-  const artists = await data.value.artists.items;
+  const artists: ArtistResponse[] = await data.value.artists.items;
   return artists.map((artist) => {
     return {
       id: artist.id,
