a9d9040b7d9ead6864935cf34cf096585762d891
yach36 <yach0306@gmail.com>
2023-06-02 00:07:53 +0900

    feat(client): define type spotify api

---
+++ client/src/pages/api/spotify/client/client.ts
@@ -0,0 +1,58 @@
+import { Result } from "./types";
+
+export type ResponseError = {
+  status: number;
+  message: string;
+};
+
+const getAccess = async () => {
+  const { data, err } = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          process.env.NEXT_PUBLIC_SPOTIFY_API_CLIENT_ID +
+            ":" +
+            process.env.NEXT_PUBLIC_SPOTIFY_API_CLIENT_SECRET
+        ).toString("base64"),
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials&sj",
+  }).then(async (res) => {
+    const data = await res.json();
+    if (!res.ok) return { err: data };
+    return { data };
+  });
+  return data;
+};
+
+const resp2result = async <T>(
+  resp: Response
+): Promise<Result<T, ResponseError>> => {
+  const data = await resp.json();
+  if (!resp.ok) {
+    return {
+      type: "error",
+      error: {
+        status: resp.status,
+        message: resp.statusText,
+      },
+    };
+  }
+  return { type: "ok", value: data };
+};
+
+export const spotifyApiClient = {
+  get: async <T>(url: string) => {
+    const { access_token, token_type } = await getAccess();
+    const data = await fetch(url, {
+      cache: "no-store",
+      method: "GET",
+      headers: {
+        Authorization: `${token_type} ${access_token}`,
+      },
+    });
+    return await resp2result<T>(data);
+  },
+};
--- client/src/pages/api/spotify/search/index.ts
+++ client/src/pages/api/spotify/search/index.ts
@@ -1,17 +1,7 @@
 import { spotifyApiClient } from "../client";
 import { ArtistResponse } from "./types";
 
-export const searchArtist = async (q: string) => {
-  const data = await spotifyApiClient.get(
+export const searchArtist = async (q: string) =>
+  await spotifyApiClient.get<ArtistResponse>(
     `${process.env.NEXT_PUBLIC_SPOTIFY_API_BASE_URL}/search?q=${q}&type=artist&limit=10`
   );
-  if (data.type === "error") return data;
-  const artists: ArtistResponse[] = await data.value.artists.items;
-  return artists.map((artist) => {
-    return {
-      id: artist.id,
-      image: artist.images[1]["url"],
-      name: artist.name,
-    };
-  });
-};
