54060cf3e9da1ba495f587765e2c63cff4578a66
yach36 <yach0306@gmail.com>
2023-06-02 04:35:25 +0900

    feat(client): search page

---
+++ client/src/app/search/_styles/search.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+/* layout */
+export const containerStyle = style({
+  paddingTop: "78px",
+  paddingBottom: "56px",
+  background: "#f9f8f4",
+  minHeight: "100vh",
+});
