793578a79e286dada3330ca5fb38dd665393b118
yach36 <yach0306@gmail.com>
2023-06-02 17:50:31 +0900

    style(client): フォーマット調整

--- client/src/api/spotify/search/types/track.ts
+++ client/src/api/spotify/search/types/track.ts
@@ -25,36 +25,23 @@ type SimplifiedAlbum = {
 
 type SimplifiedTrack = {
   artists: SimplifiedArtist[];
-
   available_markets?: string[];
-
   disc_number: number;
-
   duration_ms: number;
-
   explicit: boolean;
-
   external_urls: {
     spotify: string;
   };
-
   href: string;
-
   id: string;
-
   is_local: boolean;
-
   is_playable?: boolean;
-
   linked_from?: {
     external_urls: {
       spotify: string;
     };
-
     href: string;
-
     id: string;
-
     type:
       | "user"
       | "episode"
@@ -63,38 +50,26 @@ type SimplifiedTrack = {
       | "track"
       | "album"
       | "artist";
-
     uri: string;
   };
-
   name: string;
-
   preview_url: string;
-
   restrictions: {
     reason: "market" | "product" | "explicit";
   }[];
-
   track_number: number;
-
   type: "user" | "episode" | "playlist" | "show" | "track" | "album" | "artist";
-
   uri: string;
 };
 
 type Track = SimplifiedTrack & {
   album: SimplifiedAlbum;
-
   artists: Artist[];
-
   external_ids: {
     ean: string;
-
     isrc: string;
-
     upc: string;
   };
-
   popularity: number;
 };
 
