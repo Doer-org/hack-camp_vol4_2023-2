bcc0379ac5a6f5f92cbd61e5175324595ef8dd26
yach36 <yach0306@gmail.com>
2023-06-02 18:00:50 +0900

    feat(client): 曲検索メソッド定義

--- client/src/api/spotify/search/index.ts
+++ client/src/api/spotify/search/index.ts
@@ -5,3 +5,8 @@ export const searchArtist = async (q: string) =>
   await spotifyApiClient.get<ArtistsResponse>(
     `${process.env.NEXT_PUBLIC_SPOTIFY_API_BASE_URL}/search?q=${q}&type=artist&limit=10`
   );
+
+export const searchTrack = async (q: string) =>
+  await spotifyApiClient.get(
+    `${process.env.NEXT_PUBLIC_SPOTIFY_API_BASE_URL}/search?q=${q}&type=track&limit=10`
+  );
