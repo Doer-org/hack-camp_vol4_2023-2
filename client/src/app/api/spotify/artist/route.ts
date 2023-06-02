6d3dde98611093cf66f09a557cddafaed91de0f8
yach36 <yach0306@gmail.com>
2023-06-02 16:52:53 +0900

    chore(client): 無駄ページ削除

--- client/src/app/api/spotify/artist/route.ts
+++
@@ -1,7 +0,0 @@
-import { readArtist } from "@/api/spotify/artist";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const artist = await readArtist("0TnOYISbd1XYRBk9myaseg");
-  return NextResponse.json(artist);
-}
--- client/src/app/api/spotify/search/route.ts
+++
@@ -1,7 +0,0 @@
-import { searchArtist } from "@/api/spotify/search";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const artists = await searchArtist("bump");
-  return NextResponse.json(artists);
-}
