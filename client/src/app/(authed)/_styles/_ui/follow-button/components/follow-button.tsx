3f7676623273b0cb38e565e27c0357797543b289
yach36 <yach0306@gmail.com>
2023-06-03 16:30:24 +0900

    feat(client): フォローボタンコンポーネント作成

---
+++ client/src/app/(authed)/_styles/_ui/follow-button/components/follow-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "@/ui";
+import { User } from "@/utils";
+
+type Props = {
+  from: User;
+  to: User;
+};
+
+const _FollowButton = ({ from, to }: Props) => {
+  const [following, setFollowing] = useState<boolean>();
+
+  useEffect(() => {
+    // フォローを更新する処理
+  }, [following]);
+
+  const handleClick = () => {
+    setFollowing((prev) => !prev);
+  };
+
+  return (
+    <Button color="black" onClick={handleClick}>
+      {following ? "フォロー中" : "フォローする"}
+    </Button>
+  );
+};
+
+export const FollowButton = _FollowButton;
