cf7d1701a21609a0d8e6060c629b719758873832
yuuugaaa <yuuugaaa.321@gmail.com>
2023-06-03 14:32:34 +0900

    top pageを作成

--- client/src/app/_styles/top.css.ts
+++ client/src/app/_styles/top.css.ts
@@ -19,7 +19,6 @@ export const logo = style({
   position: "absolute",
   top: "-32px",
   left: "15px",
-  zIndex: -1,
 });
 
 export const catchphrase = style({
@@ -46,13 +45,82 @@ export const textPink = style({
   fontWeight: "700",
 });
 
-export const bgBeige = {
+export const bgBeige = style({
   backgroundColor: "#F9F8F4",
-};
+});
+
+export const bgPink = style({
+  backgroundColor: "#FFEFF0",
+});
 
 export const sectionTitle = style({
   margin: "0 0 36px",
   textAlign: "center",
   fontSize: "24px",
   fontWeight: "700",
+});
+
+export const introduction = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "24px",
+  padding: "0 24px",
+});
+
+export const introductionImage = style({
+  width: "90px",
+});
+
+export const introductionText = style({
+  margin: "0",
+  fontSize: "14px",
+});
+
+export const creators = style({
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  gap: "24px",
+});
+
+export const doer = style({
+  textAlign: "center",
+});
+
+export const doerImage = style({
+  width: "150px",
+  marginBottom: "24px",
+});
+
+export const doerCatchphrase = style({
+  fontSize: "18px",
+  fontWeight: 400,
+  margin: "0 0 24px",
+});
+
+export const doerDescription = style({
+  fontSize: "14px",
+  fontWeight: 400,
+  margin: "0 0 24px",
+});
+
+export const doerSns = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "12px",
+});
+
+export const footer = style({
+  fontSize: "12px",
+  fontWeight: 700,
+  textAlign: "center",
+  color: "#FFF",
+  backgroundColor: "#0B002A",
+  padding: "12px 24px",
+});
+
+export const footerText = style({
+  margin: 0,
 });
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 import { env } from "@/utils/env";
-import { TopHeader } from "./_ui";
+import { MemberCard, TopHeader } from "./_ui";
 import * as styles from "./_styles/top.css";
-import { Button, Logo } from "@/ui";
+import { Button, Logo, SnsIcon } from "@/ui";
 
 export default function Home() {
   const { clientURL } = env();
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <TopHeader />
       <main className={styles.wrapperStyle}>
-        <div className={styles.contentStyle}>
+        <section className={styles.contentStyle}>
           <div className={styles.catchphraseArea}>
             <div className={styles.logo}>
               <Logo size="large" />
@@ -33,15 +33,136 @@ export default function Home() {
           <div className={styles.centering}>
             <Button color="pink">始める</Button>
           </div>
-        </div>
-        <div className={styles.contentStyle}>
+        </section>
+        <section className={[styles.contentStyle, styles.bgBeige].join(" ")}>
           <h3 className={styles.sectionTitle}>
             あなただけのオリジナルの
             <br />
             プロフィールを作成
           </h3>
-        </div>
+          <div className={styles.introduction}>
+            <img
+              className={styles.introductionImage}
+              src="assets/myprofile.png"
+              alt="プロフィールページ"
+            />
+            <p className={styles.introductionText}>
+              アカウント登録後、あなただけのオリジナルのプロフィールを作成することができます。
+              <br />
+              音楽や本など、自分の好きなジャンルにおけるベスト3を紹介しましょう！
+            </p>
+          </div>
+        </section>
+        <section className={styles.contentStyle}>
+          <h3 className={styles.sectionTitle}>
+            友達のプロフィールに
+            <br />
+            フォローやいいねができる
+          </h3>
+          <div className={styles.introduction}>
+            <p className={styles.introductionText}>
+              友達のプロフィールを見て、面白い！いいね！と思ったら、フォローやいいねをすることができます。
+              <br />
+              積極的にいいねして友達との交流を深めましょう！
+            </p>
+            <img
+              className={styles.introductionImage}
+              src="assets/othersprofile.png"
+              alt="プロフィールページ"
+            />
+          </div>
+        </section>
+        <section className={[styles.contentStyle, styles.bgBeige].join(" ")}>
+          <h3 className={styles.sectionTitle}>
+            プロフィールの変更は
+            <br />
+            タイムラインから確認
+          </h3>
+          <div className={styles.introduction}>
+            <img
+              className={styles.introductionImage}
+              src="assets/timeline.png"
+              alt="プロフィールページ"
+            />
+            <p className={styles.introductionText}>
+              フォローしたユーザーのプロフィールが更新されたときは、タイムラインから確認することができます。
+              <br />
+              友達の好みの変化も、いち早く確認しましょう！
+            </p>
+          </div>
+        </section>
+        <section className={[styles.contentStyle, styles.bgPink].join(" ")}>
+          <h3 className={styles.sectionTitle}>Creators</h3>
+          <div className={styles.creators}>
+            <MemberCard
+              icon="https://avatars.githubusercontent.com/u/55625375?v=4"
+              name="Aoki"
+              role="Backend/ Infra"
+              github="RyushiAok"
+              twitter=""
+            />
+            <MemberCard
+              icon="https://avatars.githubusercontent.com/u/134787738?s=60&v=4"
+              name="Ayano"
+              role="Backend/ Frontend"
+              github="mashumarrow"
+              twitter=""
+            />
+            <MemberCard
+              icon="https://avatars.githubusercontent.com/u/86759515?v=4"
+              name="yasuda"
+              role="Frontend"
+              github="yach36"
+              twitter="Quarter_st"
+            />
+            <MemberCard
+              icon="https://avatars.githubusercontent.com/u/134147341?s=60&v=4"
+              name="さき"
+              role="Frontend"
+              github="Saki-299"
+              twitter=""
+            />
+            <MemberCard
+              icon="https://avatars.githubusercontent.com/u/113420384?v=4"
+              name="yuga"
+              role="Frontend/ Design"
+              github="yuuugaaa"
+              twitter="____yuuugaaa"
+            />
+          </div>
+        </section>
+        <section className={[styles.contentStyle, styles.doer].join(" ")}>
+          <img
+            className={styles.doerImage}
+            src="https://images-ext-2.discordapp.net/external/qtfcHSSS-dP6AuQM5RKS5QFHCPsPyFtU5TqRAifP8AE/https/doer.vercel.app/og_img.png"
+            alt="Do'er"
+          />
+          <p className={styles.doerCatchphrase}>
+            同志社大学・同志社女子大学
+            <br />
+            エンジニアのためのコミュニティ
+          </p>
+          <p className={styles.doerDescription}>
+            Do&apos;erは同志社大学・同志社女子大学の学生が所属するエンジニアサークルです。
+            <br />
+            技術のキャッチアップだけでなく、ハッカソン出場やチームメンバーの育成などキャリアに繋げられる活動を行っています。
+          </p>
+          <div className={styles.doerSns}>
+            <a href="https://github.com/Doer-org">
+              <SnsIcon sns="github" />
+            </a>
+            <a href="https://twitter.com/du_doer">
+              <SnsIcon sns="twitter" />
+            </a>
+            <a href="https://doer.vercel.app/">
+              <SnsIcon sns="doer" />
+            </a>
+          </div>
+        </section>
       </main>
+      <footer className={styles.footer}>
+        <p className={styles.footerText}>TomoShiru</p>
+      </footer>
     </>
   );
 }
