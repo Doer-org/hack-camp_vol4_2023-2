ee77e25a3fcdc16240cd71c51186de55e8b50a72
yach36 <yach0306@gmail.com>
2023-06-03 13:23:53 +0900

    feat(client): timeline/:id ページを作成

--- client/src/app/(authed)/timeline/page.tsx
+++ client/src/app/(authed)/timeline/page.tsx
@@ -1,44 +1,5 @@
 import React from "react";
-import { NoticeCard } from "../../_ui";
-import * as styles from "./_styles/timeline.css";
-import { Label } from "@/ui";
-
-const user = {
-  id: 1,
-  icon: "https://avatars.githubusercontent.com/u/55625375?v=4",
-  name: "Aoki",
-};
 
 export default function Page() {
-  return (
-    <div>
-      <div className={styles.noticeStyle}>
-        <NoticeCard user={user} action="本プロフィールの内容を変更" />
-      </div>
-      <div className={styles.noticeStyle}>
-        <NoticeCard user={user} action="本プロフィールの内容を変更" />
-      </div>
-      <div className={styles.labelStyle}>
-        <Label />
-      </div>
-      <div className={styles.noticeStyle}>
-        <NoticeCard user={user} action="本プロフィールの内容を変更" />
-      </div>
-      <div className={styles.noticeStyle}>
-        <NoticeCard user={user} action="本プロフィールの内容を変更" />
-      </div>
-      <div className={styles.noticeStyle}>
-        <NoticeCard user={user} action="本プロフィールの内容を変更" />
-      </div>
-      <div className={styles.noticeStyle}>
-        <NoticeCard user={user} action="本プロフィールの内容を変更" />
-      </div>
-      <div className={styles.noticeStyle}>
-        <NoticeCard user={user} action="本プロフィールの内容を変更" />
-      </div>
-      <div className={styles.noticeStyle}>
-        <NoticeCard user={user} action="本プロフィールの内容を変更" />
-      </div>
-    </div>
-  );
+  return <>Timeline Page</>;
 }
