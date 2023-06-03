fbaffcbd1dfd804bc25e969eb5b1c6311d2c7fa5
yach36 <yach0306@gmail.com>
2023-06-04 01:28:53 +0900

    feat(client): edit-recom-card作成

---
+++ client/src/app/(authed)/profile/[id]/edit/_ui/edit-recom-card/components/edit-recom-card.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { Card } from "@/ui";
+import { Ranking } from "@/ui/ranking/components";
+import React, { useEffect, useState } from "react";
+import * as styles from "../styles/edit-recom-card.css";
+import { getAccessToken, getFavoriteArtist, readArtist } from "@/api";
+import { User } from "@/utils";
+
+type Props = {
+  contentType: "music" | "book" | "person";
+  contentName: string;
+  firstImage?: string;
+  secondImage?: string;
+  thirdImage?: string;
+  firstOnClick?: () => void;
+  secondOnClick?: () => void;
+  thirdOnClick?: () => void;
+  manageActive?: boolean;
+  user: User | null;
+};
+
+const _EditRecomCard = ({
+  contentType,
+  contentName,
+  firstImage,
+  secondImage,
+  thirdImage,
+  firstOnClick,
+  secondOnClick,
+  thirdOnClick,
+  manageActive,
+  user,
+}: Props) => {
+  const [fImage, setFImage] = useState<string | undefined>(firstImage);
+  const [sImage, setSImage] = useState<string | undefined>(secondImage);
+  const [tImage, setTImage] = useState<string | undefined>(thirdImage);
+
+  useEffect(() => {
+    (async () => {
+      const artists = await getFavoriteArtist(user?.user_id || "0");
+      const first = artists?.filter((a) => a.rank === 1)[0];
+      const second = artists?.filter((a) => a.rank === 2)[0];
+      const third = artists?.filter((a) => a.rank === 3)[0];
+
+      const token = await getAccessToken();
+      if (first) {
+        const sArtist = await readArtist(first.artist, token);
+        if (sArtist?.type !== "error") setFImage(sArtist?.value.images[2].url);
+      }
+      if (second) {
+        const sArtist = await readArtist(second.artist, token);
+        if (sArtist?.type !== "error") setSImage(sArtist?.value.images[2].url);
+      }
+      if (third) {
+        const sArtist = await readArtist(third.artist, token);
+        if (sArtist?.type !== "error") setTImage(sArtist?.value.images[2].url);
+      }
+    })();
+  }, []);
+
+  return (
+    <Card>
+      <p className={styles.titleStyle}>おすすめの{contentName}　ベスト3</p>
+      <div className={styles.itemListStyle}>
+        <div onClick={firstOnClick}>
+          <Ranking
+            contentType={contentType}
+            image={fImage}
+            rank={1}
+            manageActive={manageActive}
+          />
+        </div>
+        <div onClick={secondOnClick}>
+          <Ranking
+            contentType={contentType}
+            image={sImage}
+            rank={2}
+            manageActive={manageActive}
+          />
+        </div>
+        <div onClick={thirdOnClick}>
+          <Ranking
+            contentType={contentType}
+            image={tImage}
+            rank={3}
+            manageActive={manageActive}
+          />
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export const EditRecomCard = _EditRecomCard;
