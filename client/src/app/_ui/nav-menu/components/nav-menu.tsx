c04f3475b13a88622fa004a9db0cd1d495fe170f
yach36 <yach0306@gmail.com>
2023-06-04 12:09:17 +0900

    feat: nev-menu me取得

--- client/src/app/_ui/nav-menu/components/nav-menu.tsx
+++ client/src/app/_ui/nav-menu/components/nav-menu.tsx
@@ -1,33 +1,60 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import * as styles from "../styles/nav-menu.css";
 import Link from "next/link";
+import { fetchAuthInfo } from "@/api/auth";
+
+type Me = {
+  nickname: string;
+  name: string;
+  picture: string;
+  updated_at: string;
+  sub: string;
+  sid: string;
+};
 
 const _NavMenu = () => {
+  const [me, setMe] = useState<Me>();
+
+  useEffect(() => {
+    (async () => {
+      const authInfo = await fetchAuthInfo();
+      const me = authInfo.me;
+      setMe(me);
+    })();
+  }, []);
+
   return (
     <div className={styles.frameStyle}>
       <ul className={styles.innerStyle}>
         <li className={styles.itemStyle}>
-          <Link href="" className={styles.linkStyle}>
+          <Link href={`/profile/${me?.sid}`} className={styles.linkStyle}>
             プロフィール
           </Link>
         </li>
         <li className={styles.itemStyle}>
-          <Link href="" className={styles.linkStyle}>
+          <Link href={`/notification/${me?.sid}`} className={styles.linkStyle}>
             通知
           </Link>
         </li>
         <li className={styles.itemStyle}>
-          <Link href="" className={styles.linkStyle}>
+          <Link
+            href={`/profile/follow/${me?.sid}`}
+            className={styles.linkStyle}
+          >
             フォロー
           </Link>
         </li>
         <li className={styles.itemStyle}>
-          <Link href="" className={styles.linkStyle}>
+          <Link
+            href={`/profile/follower/${me?.sid}`}
+            className={styles.linkStyle}
+          >
             フォロワー
           </Link>
         </li>
         <li className={styles.itemStyle}>
-          <Link href="" className={styles.linkStyle}>
+          <Link href={`/bookmark/${me?.sid}`} className={styles.linkStyle}>
             いいねしたプロフィール
           </Link>
         </li>
