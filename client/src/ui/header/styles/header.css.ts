a4f9c74e97933f302daba5295418d19026389390
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-10 23:00:57 +0900

    fix(header): localでvanilla-extractによるスタイリングが効かない問題に対処

--- client/src/ui/header/styles/header.css.ts
+++ client/src/ui/header/styles/header.css.ts
@@ -1,10 +1,15 @@
 import { style, styleVariants } from "@vanilla-extract/css";
 
 export const headerStyle = style({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+
   width: "100%",
   padding: "12px 24px",
   background: "#fff",
-  position: "fixed",
+  // position: "fixed",
   top: "0",
   left: "0",
   boxSizing: "border-box",
